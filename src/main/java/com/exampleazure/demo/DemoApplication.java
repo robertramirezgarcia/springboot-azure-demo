f3ba684032a1967d5323eaec1cd1a13540da2b25
Robert David Ramirez Garcia <robert.ramirez.garcia@gmail.com>
2022-04-01 14:15:38 -0500

    add sources

---
+++ src/main/java/com/exampleazure/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.exampleazure.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	@GetMapping("/message")
+	public String message(@RequestHeader("Request-ID") String requestId){
+		return "congrats  [" + requestId + "]";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
